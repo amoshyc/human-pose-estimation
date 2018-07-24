fc91d5f093167449645fc8f3f9cc2add217a4894
amoshyc <amoshuangyc@gmail.com>
2018-07-24 15:06:10 +0800

    Fix bugs and Add heatmap

--- coco.py
+++ coco.py
@@ -47,6 +47,7 @@ class COCOKeypoint:
         viss = np.zeros((n_people * 17, ), dtype=np.int32)
         tags = np.zeros((n_people * 17, ), dtype=np.int32)
         boxs = np.zeros((n_people, 4), dtype=np.float32)
+        lbls = np.zeros((17, dstH, dstW), dtype=np.float32)
         for tag, (kpt, box) in enumerate(self.annos[img_id]):
             kpt = np.float32(kpt).reshape(-1, 3)
             vis = kpt[:, 2]
@@ -58,16 +59,38 @@ class COCOKeypoint:
             boxs[tag] = np.float32(box)
 
         norm_term = np.float32([dstW / srcW, dstH / srcH])
-        kpts = np.round(kpts * norm_term)
+        kpts = np.round(kpts * norm_term).astype(np.int32)
         norm_term = np.hstack([norm_term, norm_term])
-        boxs = np.round(boxs * norm_term)
+        boxs = np.round(boxs * norm_term).astype(np.int32)
 
-        img = F.to_tensor(img)
-        kpts = torch.from_numpy(kpts)
-        viss = torch.from_numpy(viss)
-        tags = torch.from_numpy(tags)
-        boxs = torch.from_numpy(boxs)
-        return img, kpts, viss, tags, boxs
+        for k in range(17):
+            for pos in kpts[k::17]:
+                pos = pos[::-1].astype(np.int32)
+                rr, cc, g = self.gaussian(pos, [3, 3], shape=(dstH, dstW))
+                lbls[k, rr, cc] = np.maximum(lbls[k, rr, cc], g / g.max())
+
+        img = F.to_tensor(img)          # (3, dstH, dstW) float
+        kpts = torch.from_numpy(kpts)   # (n_people * 17, 2) int
+        viss = torch.from_numpy(viss)   # (n_poeple * 17,) int
+        tags = torch.from_numpy(tags)   # (n_people * 17,) int
+        boxs = torch.from_numpy(boxs)   # (n_people, 4) int
+        lbls = torch.from_numpy(lbls)   # (17, dstH, dstW) float
+        return img, kpts, viss, tags, boxs, lbls
+
+    @staticmethod
+    def gaussian(mu, sigma, shape=None):
+        (r, c), (sr, sc), (H, W) = mu, sigma, shape
+        rr = np.arange(r - 3 * sr, r + 3 * sr + 1)
+        cc = np.arange(c - 3 * sc, c + 3 * sc + 1)
+        rr = rr[(rr >= 0) & (rr < H)]
+        cc = cc[(cc >= 0) & (cc < W)]
+        gr = np.exp(-0.5 * ((rr - r) / sr)**2) / (np.sqrt(2 * np.pi) * sr)
+        gc = np.exp(-0.5 * ((cc - c) / sc)**2) / (np.sqrt(2 * np.pi) * sc)
+        g = np.outer(gr, gc).ravel()
+        R, C = len(rr), len(cc)
+        rr = np.broadcast_to(rr.reshape(R, 1), (R, C)).ravel()
+        cc = np.broadcast_to(cc.reshape(1, C), (R, C)).ravel()
+        return rr, cc, g
 
     @staticmethod
     def collate_fn(batch):
@@ -76,18 +99,20 @@ class COCOKeypoint:
         vis_batch = list([datum[2] for datum in batch])
         tag_batch = list([datum[3] for datum in batch])
         box_batch = torch.stack([datum[4] for datum in batch], dim=0)
-        return img_batch, kpt_batch, vis_batch, tag_batch, box_batch
+        lbl_batch = torch.stack([datum[5] for datum in batch], dim=0)
+        return img_batch, kpt_batch, vis_batch, tag_batch, box_batch, lbl_batch
 
     @staticmethod
-    def plot(img, kpts, viss, tags, boxs):
+    def plot(img, kpts, viss, tags, boxs, ax=None):
         kpts = kpts.numpy()
         viss = viss.numpy()
         tags = tags.numpy()
         boxs = boxs.numpy()
         n_person = len(boxs)
 
-        fig, ax = plt.subplots(dpi=100)
+        # image
         ax.imshow(F.to_pil_image(img))
+        ax.axis('off')
 
         # constants
         index = dict((x, i) for i, x in enumerate([
@@ -144,23 +169,22 @@ class COCOKeypoint:
             rect = mpl.patches.Rectangle((x, y), w, h, fill=False, ec=c, lw=1)
             ax.add_patch(rect)
 
-        ax.axis('off')
-        fig.tight_layout()
-        plt.show()
-
 
 if __name__ == '__main__':
     img_dir = '/store/COCO/val2017/'
     anno_path = '/store/COCO/annotations/person_keypoints_val2017.json'
     ds = COCOKeypoint(img_dir, anno_path)
 
-    img, kpts, viss, tags, boxs = ds[2]
-    ds.plot(img, kpts, viss, tags, boxs)
+    # img, kpts, viss, tags, boxs, lbls = ds[2]
+    # fig, ax = plt.subplots(dpi=100)
+    # ds.plot(img, kpts, viss, tags, boxs, ax=ax[0])
+    # plt.show()
 
-    # loader = DataLoader(ds, batch_size=2, shuffle=False, collate_fn=COCOKeypoint.collate_fn)
-    # for img_batch, kpt_batch, vis_batch, tag_batch, box_batch in iter(loader):
-    #     print(img_batch.size())
-    #     print(len(kpt_batch))
-    #     print(len(tag_batch))
-    #     print(box_batch.size())
-    #     break
+    loader = DataLoader(ds, batch_size=2, shuffle=False, collate_fn=COCOKeypoint.collate_fn)
+    for img_batch, kpt_batch, vis_batch, tag_batch, box_batch, lbl_batch in iter(loader):
+        print(img_batch.size())
+        print(len(kpt_batch))
+        print(len(tag_batch))
+        print(box_batch.size())
+        print(lbl_batch.size())
+        break
